736833fb3f3eaa9d672dacfa4f0abf85a6d74680
logikgate <nick.fredricks@flypaper.com>
2019-10-29 10:23:04 -0400

    Linting

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 (function () {
-    const dotenvExpand = require('./lib/main');
-    const env = require('dotenv').config(
-      Object.assign(
-        {},
-        require('dotenv/lib/env-options'),
-        require('dotenv/lib/cli-options')(process.argv)
-      )
+  const dotenvExpand = require('./lib/main')
+  const env = require('dotenv').config(
+    Object.assign(
+      {},
+      require('dotenv/lib/env-options'),
+      require('dotenv/lib/cli-options')(process.argv)
     )
+  )
 
-    return dotenvExpand(env)
-  })()
+  return dotenvExpand(env)
+})()
