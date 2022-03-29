01735ae0f7d02779404beb678be2b98e120f1faf
Federico Zivolo <federico.zivolo@gmail.com>
2022-03-29 20:19:32 +0200

    fix: support special characters in default expansion

--- tests/main.js
+++ tests/main.js
@@ -2,7 +2,7 @@
 'use strict'
 
 require('should')
-const Lab = require('lab')
+const Lab = require('@hapi/lab')
 const lab = exports.lab = Lab.script()
 const it = lab.test
 const describe = lab.experiment
@@ -12,15 +12,14 @@ const dotenvExpand = require('../lib/main')
 
 describe('dotenv-expand', function () {
   describe('unit tests', function () {
-    it('returns object', function (done) {
+    it('returns object', function () {
       const dotenv = { parsed: {} }
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.should.be.an.instanceOf(Object)
-      done()
     })
 
-    it('expands environment variables', function (done) {
+    it('expands environment variables', function () {
       const dotenv = {
         parsed: {
           BASIC: 'basic',
@@ -32,10 +31,9 @@ describe('dotenv-expand', function () {
 
       obj.BASIC_EXPAND.should.eql('basic')
       obj.BASIC_EXPAND_SIMPLE.should.eql('basic')
-      done()
     })
 
-    it('expands environment variables existing already on the machine', function (done) {
+    it('expands environment variables existing already on the machine', function () {
       process.env.MACHINE = 'machine'
       const dotenv = {
         parsed: {
@@ -47,10 +45,9 @@ describe('dotenv-expand', function () {
 
       obj.MACHINE_EXPAND.should.eql('machine')
       obj.MACHINE_EXPAND_SIMPLE.should.eql('machine')
-      done()
     })
 
-    it('expands missing environment variables to an empty string', function (done) {
+    it('expands missing environment variables to an empty string', function () {
       const dotenv = {
         parsed: {
           UNDEFINED_EXPAND: '$UNDEFINED_ENV_KEY'
@@ -59,10 +56,9 @@ describe('dotenv-expand', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.UNDEFINED_EXPAND.should.eql('')
-      done()
     })
 
-    it('prioritizes machine key expansion over .env', function (done) {
+    it('prioritizes machine key expansion over .env', function () {
       process.env.MACHINE = 'machine'
       const dotenv = {
         parsed: {
@@ -73,10 +69,9 @@ describe('dotenv-expand', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.MACHINE_EXPAND.should.eql('machine')
-      done()
     })
 
-    it('does not expand escaped variables', function (done) {
+    it('does not expand escaped variables', function () {
       const dotenv = {
         parsed: {
           ESCAPED_EXPAND: '\\$ESCAPED'
@@ -85,10 +80,9 @@ describe('dotenv-expand', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.ESCAPED_EXPAND.should.eql('$ESCAPED')
-      done()
     })
 
-    it('does not expand inline escaped dollar sign', function (done) {
+    it('does not expand inline escaped dollar sign', function () {
       const dotenv = {
         parsed: {
           INLINE_ESCAPED_EXPAND: 'pa\\$\\$word'
@@ -97,10 +91,9 @@ describe('dotenv-expand', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.INLINE_ESCAPED_EXPAND.should.eql('pa$$word')
-      done()
     })
 
-    it('does not overwrite preset variables', function (done) {
+    it('does not overwrite preset variables', function () {
       process.env.SOME_ENV = 'production'
       const dotenv = {
         parsed: {
@@ -110,10 +103,9 @@ describe('dotenv-expand', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.SOME_ENV.should.eql('production')
-      done()
     })
 
-    it('does not expand inline escaped dollar sign', function (done) {
+    it('does not expand inline escaped dollar sign', function () {
       const dotenv = {
         parsed: {
           INLINE_ESCAPED_EXPAND_BCRYPT: '\\$2b\\$10\\$OMZ69gxxsmRgwAt945WHSujpr/u8ZMx.xwtxWOCMkeMW7p3XqKYca'
@@ -122,10 +114,9 @@ describe('dotenv-expand', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.INLINE_ESCAPED_EXPAND_BCRYPT.should.eql('$2b$10$OMZ69gxxsmRgwAt945WHSujpr/u8ZMx.xwtxWOCMkeMW7p3XqKYca')
-      done()
     })
 
-    it('handle mixed values', function (done) {
+    it('handle mixed values', function () {
       const dotenv = {
         parsed: {
           PARAM1: '42',
@@ -135,123 +126,106 @@ describe('dotenv-expand', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.MIXED_VALUES.should.eql('$this42$is42')
-      done()
     })
   })
 
   describe('integration', function () {
     let dotenv
 
-    beforeEach(function (done) {
+    beforeEach(function () {
       dotenv = require('dotenv').config({ path: './tests/.env', debug: false })
-
-      done()
     })
 
-    it('expands environment variables', function (done) {
+    it('expands environment variables', function () {
       dotenvExpand.expand(dotenv)
 
       process.env.BASIC_EXPAND.should.eql('basic')
-      done()
     })
 
-    it('expands environment variables existing already on the machine', function (done) {
+    it('expands environment variables existing already on the machine', function () {
       process.env.MACHINE = 'machine'
       dotenvExpand.expand(dotenv)
 
       process.env.MACHINE_EXPAND.should.eql('machine')
-      done()
     })
 
-    it('expands missing environment variables to an empty string', function (done) {
+    it('expands missing environment variables to an empty string', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.UNDEFINED_EXPAND.should.eql('')
-      done()
     })
 
-    it('expands environment variables existing already on the machine even with a default', function (done) {
+    it('expands environment variables existing already on the machine even with a default', function () {
       process.env.MACHINE = 'machine'
       dotenvExpand.expand(dotenv)
 
       process.env.DEFINED_EXPAND_WITH_DEFAULT.should.eql('machine')
-      done()
     })
 
-    it('expands environment variables existing already on the machine even with a default when nested', function (done) {
+    it('expands environment variables existing already on the machine even with a default when nested', function () {
       process.env.MACHINE = 'machine'
       dotenvExpand.expand(dotenv)
 
       process.env.DEFINED_EXPAND_WITH_DEFAULT_NESTED.should.eql('machine')
-      done()
     })
 
-    it('expands environment variables undefined with one already on the machine even with a default when nested', function (done) {
+    it('expands environment variables undefined with one already on the machine even with a default when nested', function () {
       process.env.MACHINE = 'machine'
       dotenvExpand.expand(dotenv)
 
       process.env.UNDEFINED_EXPAND_WITH_DEFINED_NESTED.should.eql('machine')
-      done()
     })
 
-    it('expands missing environment variables to an empty string but replaces with default', function (done) {
+    it('expands missing environment variables to an empty string but replaces with default', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.UNDEFINED_EXPAND_WITH_DEFAULT.should.eql('default')
-      done()
     })
 
-    it('expands missing environment variables to an empty string but replaces with default nested', function (done) {
+    it('expands missing environment variables to an empty string but replaces with default nested', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.UNDEFINED_EXPAND_WITH_DEFAULT_NESTED.should.eql('default')
-      done()
     })
 
-    it('expands missing environment variables to an empty string but replaces with default nested twice', function (done) {
+    it('expands missing environment variables to an empty string but replaces with default nested twice', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.UNDEFINED_EXPAND_WITH_DEFAULT_NESTED_TWICE.should.eql('default')
-      done()
     })
 
-    it('prioritizes machine key expansion over .env', function (done) {
+    it('prioritizes machine key expansion over .env', function () {
       process.env.MACHINE = 'machine'
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.MACHINE_EXPAND.should.eql('machine')
-      done()
     })
 
-    it('multiple expand', function (done) {
+    it('multiple expand', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.MONGOLAB_URI.should.eql('mongodb://username:password@abcd1234.mongolab.com:12345/heroku_db')
-      done()
     })
 
-    it('should expand recursively', function (done) {
+    it('should expand recursively', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.MONGOLAB_URI_RECURSIVELY.should.eql('mongodb://username:password@abcd1234.mongolab.com:12345/heroku_db')
-      done()
     })
 
-    it('multiple expand', function (done) {
+    it('multiple expand', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.WITHOUT_CURLY_BRACES_URI.should.eql('mongodb://username:password@abcd1234.mongolab.com:12345/heroku_db')
-      done()
     })
 
-    it('should expand recursively', function (done) {
+    it('should expand recursively', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
       obj.WITHOUT_CURLY_BRACES_URI_RECURSIVELY.should.eql('mongodb://username:password@abcd1234.mongolab.com:12345/heroku_db')
-      done()
     })
 
-    it('should not write to process.env if ignoreProcessEnv is set', function (done) {
+    it('should not write to process.env if ignoreProcessEnv is set', function () {
       const dotenv = {
         ignoreProcessEnv: true,
         parsed: {
@@ -263,15 +237,31 @@ describe('dotenv-expand', function () {
       const evaluation = typeof process.env.SHOULD_NOT_EXIST
       obj.SHOULD_NOT_EXIST.should.eql('testing')
       evaluation.should.eql('undefined')
-      done()
     })
 
-    it('should expand with default value correctly', function (done) {
+    it('expands environment variables existing already on the machine even with a default with special characters', function () {
       const obj = dotenvExpand.expand(dotenv).parsed
 
-      obj.UNDEFINED_EXPAND_WITH_DEFAULT_WITH_SPECIAL_CHARACTERS.should.eql('/default/path')
-      obj.WITHOUT_CURLY_BRACES_UNDEFINED_EXPAND_WITH_DEFAULT_WITH_SPECIAL_CHARACTERS.should.eql('/default/path')
-      done()
+      obj.DEFINED_EXPAND_WITH_DEFAULT_WITH_SPECIAL_CHARACTERS.should.eql('machine')
+    })
+
+    it('should expand with default value correctly', function () {
+      const obj = dotenvExpand.expand(dotenv).parsed
+
+      obj.UNDEFINED_EXPAND_WITH_DEFAULT_WITH_SPECIAL_CHARACTERS.should.eql(
+        '/default/path:with/colon'
+      )
+      obj.WITHOUT_CURLY_BRACES_UNDEFINED_EXPAND_WITH_DEFAULT_WITH_SPECIAL_CHARACTERS.should.eql(
+        '/default/path:with/colon'
+      )
+    })
+
+    it('should expand with default nested value correctly', function () {
+      const obj = dotenvExpand.expand(dotenv).parsed
+
+      obj.UNDEFINED_EXPAND_WITH_DEFAULT_WITH_SPECIAL_CHARACTERS_NESTED.should.eql(
+        '/default/path:with/colon'
+      )
     })
   })
 })
