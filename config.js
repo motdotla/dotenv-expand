a3c1189a6087b70b0ebda925b0dcebea22cc1db2
lucas nascimento <iam@lucasnascimen.to>
2022-06-11 18:17:02 -0300

    fixed config options when using env or args (from 20577e3baabe4cbe230809b915b9c792a8a6635a)

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
 (function () {
   const dotenvExpand = require('./lib/main').expand
 
-  const env = require('dotenv').config()
+  const env = require('dotenv').config(
+    Object.assign(
+      {},
+      require('dotenv/lib/env-options'),
+      require('dotenv/lib/cli-options')(process.argv)
+    )
+  )
 
   return dotenvExpand(env)
 })()
